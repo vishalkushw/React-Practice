d5434f7373c3731978c2eba73fd199e76b097a58
vishal kushwah <kushwahvishal2391@gmail.com>
2025-02-05 09:57:27 +0530

    react home

---
+++ React home/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./Home"
+const App=()=>{
+  return(
+    <>
+    <Home/>
+    <h1>Vishal kushwah </h1>
+    <h1>First react program </h1>
+    </>
+  )
+}
+export default App
---
+++ React home/src/Home.jsx
@@ -0,0 +1,9 @@
+
+const Home=()=>{
+    return(
+      <>
+      <h1>Vishal kushwah it is your home page</h1>
+      </>
+    )
+  }
+  export default Home
